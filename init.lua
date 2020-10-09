48dc829732ab50622c6ed9266448bf4ceb07597c
Leixb <abone9999@gmail.com>
2020-10-09 12:58:22 +0200

    Clean up and fix bugs

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-local api = vim.api
-
 --------------------------------------------------------------------------------
 -- Check if Plug is installed and download it if not
 --------------------------------------------------------------------------------
@@ -12,7 +10,7 @@ if f == nil then
 		vim_plug_install_path,
 		'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 	))
-    api.nvim_command('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+    vim.api.nvim_command('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
 else
 	f:close()
 end
@@ -77,21 +75,21 @@ local buf_opts = {
 
 for k, v in pairs(global_opts) do
 	vim.o[k] = v
-    -- api.nvim_set_option(k, v)
+    -- vim.api.nvim_set_option(k, v)
 end
 
 for k, v in pairs(buf_opts) do
 	vim.o[k] = v
 	vim.bo[k] = v
-    -- api.nvim_set_option(k, v)
-    -- api.nvim_buf_set_option(0, k, v)
+    -- vim.api.nvim_set_option(k, v)
+    -- vim.api.nvim_buf_set_option(0, k, v)
 end
 
 for k, v in pairs(win_opts) do
 	vim.o[k] = v
 	vim.wo[k] = v
-    -- api.nvim_set_option(k, v)
-    -- api.nvim_win_set_option(0, k, v)
+    -- vim.api.nvim_set_option(k, v)
+    -- vim.api.nvim_win_set_option(0, k, v)
 end
 
 --------------------------------------------------------------------------------
@@ -159,12 +157,15 @@ local nmap = {
     ['<C-l>'] = '<C-w><C-l>',
     ['<C-h>'] = '<C-w><C-h>',
 
-    ['<F1>'] = {':tabprevious<CR>', { noremap = true , silent = true}},
-    ['<F2>'] = {':bprevious!<CR>',  { noremap = true , silent = true}},
-    ['<F3>'] = {':bnext!<CR>',      { noremap = true , silent = true}},
-    ['<F4>'] = {':tabnext<CR>',     { noremap = true , silent = true}},
+    ['<F1>']  = {':tabprevious<CR>', { noremap = true , silent = true }},
+    ['<F2>']  = {':bprevious!<CR>',  { noremap = true , silent = true }},
+    ['<F3>']  = {':bnext!<CR>',      { noremap = true , silent = true }},
+    ['<F4>']  = {':tabnext<CR>',     { noremap = true , silent = true }},
+
+    ['<F12>'] = {'magg=G`a',         { noremap = true , silent = true }},
 
     ['Y'] = 'y$',
+
     ['n'] = 'nzzzv',
     ['N'] = 'Nzzzv',
 
@@ -189,7 +190,6 @@ local nmap = {
 
     ['K']          = {':call v:lua.show_documentation()<CR>', {silent = true, noremap = true}},
 
--- Remap keys for applying codeAction to the current buffer.
     ['<leader>ac'] = {'<Plug>(coc-codeaction)',              {}},
     ['<leader>qf'] = {'<Plug>(coc-fix-current)',             {}},
 
@@ -197,7 +197,7 @@ local nmap = {
 
 -- Coclist mappings
 
-    ['<leader>a']  = {':<C-u>CocList diagnostics<cr>',       {silent = true, nowait = true, noremap = true}},
+    ['<leader>d']  = {':<C-u>CocList diagnostics<cr>',       {silent = true, nowait = true, noremap = true}},
     ['<leader>e']  = {':<C-u>CocList extensions<cr>',        {silent = true, nowait = true, noremap = true}},
     ['<leader>c']  = {':<C-u>CocList commands<cr>',          {silent = true, nowait = true, noremap = true}},
     ['<leader>o']  = {':<C-u>CocList outline<cr>',           {silent = true, nowait = true, noremap = true}},
@@ -207,17 +207,22 @@ local nmap = {
     ['<leader>p']  = {':<C-u>CocListResume<CR>',             {silent = true, nowait = true, noremap = true}},
 }
 
+local jExKeymap = [[ pumvisible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? ]] ..
+[[ "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" : v:lua.check_back_space() ?]] ..
+[[ "\<F13>" : coc#refresh() ]]
+
+local confirmKeymap = [[pumvisible() ? coc#_select_confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]]
+
 local imap = {
     ['jk']        = '<ESC>',
-    ['<F13>']     = { [[ pumvisible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" : v:lua.check_back_space() ? "\<F13>" : coc#refresh() ]] , {silent = true, noremap = true, expr = true}},
-    ['<S-F13>']   = {'<C-p>',                                                                               {silent = true, noremap = true}},
-    ['<TAB>']     = {'pumvisible() ? "\\<C-n>" : "\\<TAB>"',                                                {silent = true, noremap = true, expr = true}},
-    ['<S-TAB>']   = {'pumvisible() ? "\\<C-p>" : "\\<C-h>"',                                                {silent = true, noremap = true, expr = true}},
-    ['<C-space>'] = {'coc#refresh()',                                                                       {silent = true, noremap = true, expr = true}},
-    ['<CR>']      = {'pumvisible() ? coc#_select_confirm() : "\\<C-g>u\\<CR>\\<c-r>=coc#on_enter()\\<CR>"', {silent = true, noremap = true, expr = true}},
+    ['<F13>']     = {jExKeymap,                              {silent = true, noremap = true, expr = true}},
+    ['<S-F13>']   = {'<C-p>',                                {silent = true, noremap = true}},
+    ['<TAB>']     = {'pumvisible() ? "\\<C-n>" : "\\<TAB>"', {silent = true, noremap = true, expr = true}},
+    ['<S-TAB>']   = {'pumvisible() ? "\\<C-p>" : "\\<C-h>"', {silent = true, noremap = true, expr = true}},
+    ['<C-space>'] = {'coc#refresh()',                        {silent = true, noremap = true, expr = true}},
+    ['<CR>']      = {confirmKeymap,                          {silent = true, noremap = true, expr = true}},
 }
 
--- Map function and class text objects
 local xmap = {
     ['if']        = '<Plug>(coc-funcobj-i)',
     ['af']        = '<Plug>(coc-funcobj-a)',
@@ -249,9 +254,9 @@ local default_args = { noremap = true }
 for mode, map in pairs({ n = nmap, v = vmap, t = tmap, c = cmap, i = imap, x = xmap, o = omap }) do
     for from, to in pairs(map) do
         if type(to) == 'table' then
-            api.nvim_set_keymap(mode, from, to[1], to[2])
+            vim.api.nvim_set_keymap(mode, from, to[1], to[2])
         else
-            api.nvim_set_keymap(mode, from, to, default_args)
+            vim.api.nvim_set_keymap(mode, from, to, default_args)
         end
     end
 end
@@ -267,9 +272,13 @@ local vars = {
     sonokai_enable_italic          = 0;
     sonokai_disable_italic_comment = 1;
 
+    -- netrw
+
     netrw_banner    = 0;
     netrw_liststyle = 3;
     netrw_winsize   = 30;
+
+    -- vimtex
 
     tex_flavor = 'latex';
 
@@ -293,9 +302,13 @@ local vars = {
         },
     };
 
+    -- UltiSnips
+
     UltiSnipsExpandTrigger       = '<Nop>';
     UltiSnipsJumpForwardTrigger  = '<Nop>';
     UltiSnipsJumpBackwardTrigger = '<Nop>';
+
+    -- Lightline
 
     lightline = {
         colorscheme        = 'wombat',
@@ -369,14 +382,14 @@ local vars = {
 
 for k,v in pairs(vars) do
 	vim.g[k] = v
-    -- api.nvim_set_var(k, v)
+    -- vim.api.nvim_set_var(k, v)
 end
 
 --------------------------------------------------------------------------------
 -- Colorscheme
 --------------------------------------------------------------------------------
 
-api.nvim_command('colorscheme sonokai')
+vim.api.nvim_command('colorscheme sonokai')
 
 --------------------------------------------------------------------------------
 -- augroups
@@ -402,10 +415,12 @@ local augroups = {
 }
 
 for augroup, autocmds in pairs(augroups) do
-    api.nvim_command(('augroup leixb-%s'):format(augroup))
-    api.nvim_command('autocmd!')
-    for _, autocmd in ipairs(autocmds) do api.nvim_command(('autocmd %s'):format(autocmd)) end
-    api.nvim_command('augroup end')
+    vim.api.nvim_command(('augroup leixb-%s'):format(augroup))
+    vim.api.nvim_command('autocmd!')
+    for _, autocmd in ipairs(autocmds) do
+        vim.api.nvim_command(('autocmd %s'):format(autocmd))
+    end
+    vim.api.nvim_command('augroup end')
 end
 
 --------------------------------------------------------------------------------
@@ -421,7 +436,7 @@ local commands = {
 }
 
 for _, c in pairs(commands) do
-    api.nvim_command('command! ' .. c)
+    vim.api.nvim_command('command! ' .. c)
 end
 
 
@@ -466,29 +481,8 @@ for k,v in pairs(denite_options) do
     vim.fn['denite#custom#option']('default', k, v)
 end
 
-function denite_filter_settings()
-    local bindings = {
-        ['<CR>']  = 'do_action',
-        ['<ESC>'] = 'quit',
-        ['<C-t>'] = {'do_action', 'tabopen'},
-        ['<C-v>'] = {'do_action', 'vsplit'},
-        ['<C-h>'] = {'do_action', 'split'},
-        ['.']     = 'toggle_select',
-    }
-    for k,v in pairs(bindings) do
-        denite_map('i', k, v)
-    end
-    local bindings_noexpr = {
-        ['<C-o>'] = '<Plug>(denite_filter_quit)',
-        ['jk']    = '<Plug>(denite_filter_quit)',
-    }
-    for k,v in pairs(bindings_noexpr) do
-        denite_map_noexpr(k, v)
-    end
-end
-
-function denite_window_settings()
-    local bindings = {
+local denite_mappings = {
+    normal = {
         ['<CR>']  = 'do_action',
         ['q']     = 'quit',
         ['<ESC>'] = 'quit',
@@ -500,14 +494,38 @@ function denite_window_settings()
         ['<C-v>'] = {'do_action', 'vsplit'},
         ['<C-h>'] = {'do_action', 'split'},
         ['.']     = 'toggle_select',
+    },
+    filter = {
+        ['<CR>']  = 'do_action',
+        ['<ESC>'] = 'quit',
+        ['<C-t>'] = {'do_action', 'tabopen'},
+        ['<C-v>'] = {'do_action', 'vsplit'},
+        ['<C-h>'] = {'do_action', 'split'},
+        ['.']     = 'toggle_select',
+    },
+    filter_no_expr = {
+        ['<C-o>'] = '<Plug>(denite_filter_quit)',
+        ['jk']    = '<Plug>(denite_filter_quit)',
     }
-    for k,v in pairs(bindings) do
+}
+
+function denite_filter_settings()
+    for k,v in pairs(denite_mappings.filter) do
+        denite_map('i', k, v)
+    end
+    for k,v in pairs(denite_mappings.filter_no_expr) do
+        denite_map_noexpr(k, v)
+    end
+end
+
+function denite_window_settings()
+    for k,v in pairs(denite_mappings.normal) do
         denite_map('n', k, v)
     end
 end
 
 function denite_map_noexpr(key, action)
-    api.nvim_buf_set_keymap(0, 'i', key, action, { silent = true, noremap = true })
+    vim.api.nvim_buf_set_keymap(0, 'i', key, action, { silent = true })
 end
 
 function denite_map(mode, key, action)
@@ -516,7 +534,7 @@ function denite_map(mode, key, action)
     else
         action = ("denite#do_map('%s')"):format(action)
     end
-    api.nvim_buf_set_keymap(0, mode, key, action, { silent = true, expr = true, noremap = true })
+    vim.api.nvim_buf_set_keymap(0, mode, key, action, { silent = true, expr = true, noremap = true })
 end
 
 --------------------------------------------------------------------------------
@@ -524,10 +542,10 @@ end
 --------------------------------------------------------------------------------
 
 function check_back_space()
-  local pos = api.nvim_win_get_cursor(0)
+  local pos = vim.api.nvim_win_get_cursor(0)
   if pos[2] == 0 then return true end
 
-  local line = api.nvim_get_current_line()
+  local line = vim.api.nvim_get_current_line()
   local c = string.sub(line, pos[2], pos[2])
   return vim.regex('^\\s$"'):match_str(c)
 end
@@ -535,7 +553,7 @@ end
 function show_documentation()
 	if vim.tbl_contains({'vim', 'help'}, vim.bo.filetype) then
 		local cword = vim.fn.expand('<cword>')
-		api.nvim_command('help ' .. cword)
+		vim.api.nvim_command('help ' .. cword)
 	else
 		vim.fn.CocActionAsync('doHover')
 	end
