439926bf6da117760bde0b0ebe97b808218cc4b1
Leixb <abone9999@gmail.com>
2021-09-27 16:12:56 +0200

    Clean syntax

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,7 +9,7 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.api.nvim_command 'packadd packer.nvim'
 end
 
-return require('packer').startup({function(use)
+return require'packer'.startup{function(use)
     use 'wbthomason/packer.nvim'
 
     use 'lewis6991/impatient.nvim'
@@ -20,15 +20,15 @@ return require('packer').startup({function(use)
 
     use 'tpope/vim-eunuch'
 
-    use {'junegunn/vim-easy-align', cmd = "EasyAlign"}
+    use {'junegunn/vim-easy-align', cmd = 'EasyAlign'}
 
     use 'editorconfig/editorconfig-vim'
 
-    use {'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup() end }
+    use {'windwp/nvim-autopairs', config = function() require'nvim-autopairs'.setup() end }
 
     use {'norcalli/nvim-colorizer.lua', config = function() require'colorizer'.setup() end }
 
-    use {'kwkarlwang/bufresize.nvim', config = function() require('bufresize').setup() end }
+    use {'kwkarlwang/bufresize.nvim', config = function() require'bufresize'.setup() end }
 
     -- Status lines
     use {
@@ -87,59 +87,57 @@ return require('packer').startup({function(use)
 
     -- Language specific
     use {
-        {'simrat39/rust-tools.nvim', after = "nvim-lspconfig",
-            ft = { "rust" }, event = { "BufEnter Cargo.toml" },
+        {'simrat39/rust-tools.nvim', after = 'nvim-lspconfig',
+            ft = { 'rust' }, event = { 'BufEnter Cargo.toml' },
             config = function() require'conf.rust-tools' end  ,  },
-        {'sebdah/vim-delve',             ft = "go" },
-        {'lervag/vimtex',                ft = {"tex", "latex"}},
-        {'dart-lang/dart-vim-plugin',    ft = "dart"},
-        {'JuliaEditorSupport/julia-vim', disable = true}, -- , ft = "julia" -- does not load lazily properly (https://github.com/JuliaEditorSupport/julia-vim/issues/269)
-        {'dag/vim-fish',                 ft = "fish"},
-        {'jalvesaq/Nvim-R',              branch = 'stable', ft={"r", "rmd", "rout"}},
+        {'sebdah/vim-delve',             ft = 'go' },
+        {'lervag/vimtex',                ft = {'tex', 'latex'}},
+        {'dart-lang/dart-vim-plugin',    ft = 'dart'},
+        {'JuliaEditorSupport/julia-vim', disable = true}, -- , ft = 'julia' -- does not load lazily properly (https://github.com/JuliaEditorSupport/julia-vim/issues/269)
+        {'dag/vim-fish',                 ft = 'fish'},
+        {'jalvesaq/Nvim-R',              branch = 'stable', ft={'r', 'rmd', 'rout'}},
     }
 
     -- Git
     use {
         {'tpope/vim-rhubarb', requires = 'tpope/vim-fugitive',
             cmd = {
-                "G", "Ggrep", "Git", "Glgrep", "Gclog", "Gllog", "Gcd", "Glcd",
-                "Gedit", "Gsplit", "Gvsplit", "Gtabedit", "Gpedit", "Gread",
-                "Gwrite", "Gwq", "Gdiffsplit", "GMove", "GRename", "GDelete",
-                "GRemove", "GBrowse",
+                'G', 'Ggrep', 'Git', 'Glgrep', 'Gclog', 'Gllog', 'Gcd', 'Glcd',
+                'Gedit', 'Gsplit', 'Gvsplit', 'Gtabedit', 'Gpedit', 'Gread',
+                'Gwrite', 'Gwq', 'Gdiffsplit', 'GMove', 'GRename', 'GDelete',
+                'GRemove', 'GBrowse',
             }
         },
-        {'junegunn/gv.vim',     cmd = "GV"},
-        {'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }, config = function() require('gitsigns').setup() end},
+        {'junegunn/gv.vim',     cmd = 'GV'},
+        {'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' }, config = function() require'gitsigns'.setup() end},
     }
 
     -- Spell
-    use {'rhysd/vim-grammarous', cmd = {'GrammarousCheck'}, } -- ft = {"markdown", "tex", "latex", "text"},
+    use {'rhysd/vim-grammarous', cmd = {'GrammarousCheck'}, }
     use {
         'lewis6991/spellsitter.nvim',
-        config = function() require('spellsitter').setup() end
+        config = function() require'spellsitter'.setup() end
     }
 
     -- LSP
     use {
         'neovim/nvim-lspconfig',
-        config = function() require('conf.lsp') end,
+        config = function() require'conf.lsp' end,
         requires = {
             {'kabouzeid/nvim-lspinstall', cmd = 'LspInstall'},
-            {'glepnir/lspsaga.nvim', module = 'lspsaga', config = function() require 'lspsaga'.init_lsp_saga() end },
+            {'glepnir/lspsaga.nvim', module = 'lspsaga', config = function() require'lspsaga'.init_lsp_saga() end },
             'jose-elias-alvarez/nvim-lsp-ts-utils',
             'ray-x/lsp_signature.nvim',
-            -- {'simrat39/symbols-outline.nvim', cmd = {'SymbolsOutline', 'SymbolsOutlineOpen'}},
             {'simrat39/symbols-outline.nvim', opt = false, cmd = {'SymbolsOutline', 'SymbolsOutlineOpen'}},
         },
     }
 
     -- DAP
-
     use {
-        "rcarriga/nvim-dap-ui",
+        'rcarriga/nvim-dap-ui',
         module = 'dapui',
-        requires = {"mfussenegger/nvim-dap", module = 'dap'},
-        config = function() require('conf.dap') end
+        requires = {'mfussenegger/nvim-dap', module = 'dap'},
+        config = function() require'conf.dap' end
     }
 end,
     --------------------------------------------------------------------------------
@@ -149,9 +147,9 @@ end,
     config = {
         display = {
             open_fn = function()
-                return require('packer.util').float({ border = 'single' })
+                return require'packer.util'.float({ border = 'single' })
             end
         },
         compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua',
     }
-})
+}
