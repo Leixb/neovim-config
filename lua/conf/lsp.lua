69651d70e9960ebb0d79ac3455fa7268c568792e
LeixB <abone9999@gmail.com>
2021-12-27 19:43:38 +0100

    Update sumneko lua config

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -37,41 +37,36 @@ for _,val in pairs(lsp_list) do
     }
 end
 
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
-local sumneko_root_path = vim.fn.stdpath('cache')..'/nvim_lsp/sumneko_lua/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-
-lspconfig.sumneko_lua.setup{
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    on_attach = lsp_attach,
+lspconfig.sumneko_lua.setup {
     settings = {
         Lua = {
-            runtime = { version = 'LuaJIT', path = vim.split(package.path, ';'), },
-            completion = { keywordSnippet = 'Disable', },
-            diagnostics = { enable = true, globals = {
-                'vim', 'describe', 'it', 'before_each', 'after_each' },
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
             },
             workspace = {
-                library = {
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                }
-            }
-        }
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
     capabilities = capabilities,
+    on_attach = lsp_attach,
 }
-
 
 -- Diagnostics
 
