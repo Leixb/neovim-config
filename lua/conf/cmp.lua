0fa955eb48be860656280e1c01e05ea7d0f3a4f1
LeixB <abone9999@gmail.com>
2022-03-19 01:25:22 +0100

    Remove annoying mapping

--- lua/conf/cmp.lua
+++ lua/conf/cmp.lua
@@ -12,26 +12,6 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-                -- cmp.select_next_item()
-            if luasnip and luasnip.jumpable(1) then
-                luasnip.jump(1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            -- if cmp.visible() then
-                -- cmp.select_prev_item()
-            if luasnip and luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
     },
     sources = {
         { name = 'nvim_lsp' },
