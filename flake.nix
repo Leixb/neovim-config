065cc5563081613c6438d5a2deb68a1d2d231d49
LeixB <abone9999@gmail.com>
2022-03-26 19:01:49 +0100

    Add flake devShell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+  flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            sumneko-lua-language-server
+          ];
+        };
+      }
+    );
+}
