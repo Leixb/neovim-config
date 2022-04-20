8d73af0363864621773b4282ec5844c330edaa8e
LeixB <abone9999@gmail.com>
2022-04-20 20:06:59 +0200

    Remove telescope fzf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -79,7 +79,7 @@ return require'packer'.startup{function(use)
     }
 
     use {'nvim-lua/telescope.nvim', module = 'telescope', cmd = 'Telescope',
-        requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim', {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }},
+        requires = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' },
         config = function() require'conf.telescope' end}
 
     use { 'nvim-treesitter/nvim-treesitter', config = function() require'conf.treesitter' end,
