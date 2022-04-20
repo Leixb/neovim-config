80a724b746f25c8a4a1a6cadc5e744c70cc6a592
LeixB <abone9999@gmail.com>
2022-04-20 20:06:49 +0200

    Fix cmp mappings

--- lua/conf/cmp.lua
+++ lua/conf/cmp.lua
@@ -5,14 +5,17 @@ cmp.setup({
     snippet = {
         expand = function(args) luasnip.lsp_expand(args.body) end,
     },
-    mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }),
 
-    },
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
