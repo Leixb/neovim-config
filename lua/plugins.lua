a33213c80769c31f36964256d930ed390a568ebb
LeixB <abone9999@gmail.com>
2022-06-29 15:56:53 +0200

    Remove trailing whitespace

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -179,7 +179,7 @@ return require'packer'.startup{function(use)
     -- Firenvim
     use {
         'glacambre/firenvim',
-        run = function() vim.fn['firenvim#install'](0) end 
+        run = function() vim.fn['firenvim#install'](0) end
     }
 
 end,
