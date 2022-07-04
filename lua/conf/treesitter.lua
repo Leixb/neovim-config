d7d1d8f15dbe888fb3c1cd12451b74ee2b1fd88e
LeixB <abone9999@gmail.com>
2022-07-04 15:12:36 +0200

    Remove ensure installed from tree-sitter

--- lua/conf/treesitter.lua
+++ lua/conf/treesitter.lua
@@ -1,16 +1,7 @@
 local parser_configs = require'nvim-treesitter.parsers'.get_parser_configs()
 
-parser_configs.norg = {
-    install_info = {
-        url = 'https://github.com/nvim-neorg/tree-sitter-norg',
-        files = { 'src/parser.c', 'src/scanner.cc' },
-        branch = 'main'
-    },
-}
-
 require'nvim-treesitter.configs'.setup {
     highlight = {enable = true},
-    ensure_installed = 'all',
     indent = {enable = true},
     autopairs = {enable = true},
     rainbow = {enable = true},
